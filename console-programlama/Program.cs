07ba7bdef2976eab44486ffa90f4c37609671505
Yunus Emre <yemregunen@gmail.com>
2022-12-02 00:53:31 +0300

    c# folder added

---
+++ console-programlama/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("name :");
+        String name = Console.ReadLine();
+        Console.WriteLine("surname :");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine("hi " +name+ " " + surname);
+    }
+}
