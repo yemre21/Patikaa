918ea617824d783f90a922a9afa1e484c3590c81
Yunus Emre <yemregunen@gmail.com>
2022-12-05 17:20:37 +0300

    console dosyasında bazı degisiklikler

--- console-programlama/Program.cs
+++ console-programlama/Program.cs
@@ -2,12 +2,26 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       /* Console.WriteLine("Hello, World!");
         Console.WriteLine("name :");
         String name = Console.ReadLine();
         Console.WriteLine("surname :");
         string surname = Console.ReadLine();
 
         Console.WriteLine("hi " +name+ " " + surname);
+           int degisken = 5;
+        string degisken = "merhaba";
+            Console.WriteLine(degisken);*/
+
+                    int []arr = new int[5];
+        for(int i=0;i<5;i++){
+            Console.Write("eleman giriniz: ");
+            arr[i]= int.Parse(Console.ReadLine());
+            
+        }
+        foreach(int item in arr){
+            Console.WriteLine("{0}. eleman baş",item);
+            Console.WriteLine(item+". eleman baş",item);
+        }
     }
 }
