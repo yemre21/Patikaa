714364ad24045e24ece6bb05761ad37bd541cb04
Yunus Emre <yemregunen@gmail.com>
2023-01-04 01:53:10 +0300

    Update hw4.sql

--- SQL/homeWorks/hw4.sql
+++ SQL/homeWorks/hw4.sql
@@ -1,1 +1,24 @@
-coming soon :)
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-) film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+
+-> SELECT DISTINC replacement_cost  FROM film;
+
+-) film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+
+-> SELECT COUNT(DISTINC replacement_cost) FROM film;
+
+-) film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+
+-> SELECT COUNT(*) FROM film
+-> WHERE title LIKE 'T%' AND rating = 'G';
+
+-) country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+
+-> SELECT COUNT(*) FROM country
+-> WHERE country LIKE '_____';
+
+-) city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+
+-> SELECT COUNT(*) FROM city
+-> WHERE name ILIKE '%r';
