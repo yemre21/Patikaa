44a3b6e97310e10aba5a151fa325cd620ce092ab
Yunus Emre <yemregunen@gmail.com>
2023-01-05 17:26:39 +0300

    Create hw012.sql

---
+++ SQL/homeWorks/hw012.sql
@@ -0,0 +1,42 @@
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-) film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+
+-> SELECT COUNT 
+-> ( 
+->   SELECT * FROM film
+->   WHERE lenght > AVG(lenght)
+-> )
+-> FROM film;
+
+-) film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+-> SELECT COUNT 
+-> ( 
+->   SELECT * FROM film
+->   WHERE rental_rate = MAX(rental_rate)
+-> )
+-> FROM film;
+
+-) film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+
+-> SELECT * FROM film 
+-> WHERE rental_rate = 
+-> ( 
+->   SELECT MIN(rental_rate) FROM film
+-> )
+-> AND replacement_cost =
+-> (
+-> SELECT MIN(replacement_cost) FROM film
+-> );
+
+-) payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+-> SELECT first_name, last_name FROM customer 
+-> WHERE customer_id = 
+-> ( 
+->   SELECT customer_id FROM payment
+->   WHERE amount = MAX(amount)
+-> );
+
+--- not: son sorgu düzenlenecek, şuan hatalı
